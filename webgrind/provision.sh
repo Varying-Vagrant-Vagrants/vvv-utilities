9b417cc8bfaea94907f451fcc8454ee79c54ddea
LoreleiAurora <iam@l5i.me>
2016-11-05 02:26:02 +0000

    Add Webgrind

---
+++ webgrind/provision.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Webgrind install (for viewing callgrind/cachegrind files produced by
+# xdebug profiler)
+if [[ ! -d "/srv/www/default/webgrind" ]]; then
+    echo -e "\nDownloading webgrind, see https://github.com/michaelschiller/webgrind.git"
+    git clone "https://github.com/michaelschiller/webgrind.git" "/srv/www/default/webgrind"
+else
+    echo -e "\nUpdating webgrind..."
+    cd /srv/www/default/webgrind
+    git pull --rebase origin master
+fi
