1b20d1004d67b1469121a75224ac12ca6229eba0
LoreleiAurora <iam@l5i.me>
2016-11-05 01:41:11 +0000

    Add Memcached admin

---
+++ memcached-admin/provision.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Download and extract phpMemcachedAdmin to provide a dashboard view and
+# admin interface to the goings on of memcached when running
+if [[ ! -d "/srv/www/default/memcached-admin" ]]; then
+	echo -e "\nDownloading phpMemcachedAdmin, see https://github.com/wp-cloud/phpmemcacheadmin"
+	cd /srv/www/default
+	wget -q -O phpmemcachedadmin.tar.gz "https://github.com/wp-cloud/phpmemcacheadmin/archive/1.2.2.1.tar.gz"
+	tar -xf phpmemcachedadmin.tar.gz
+	mv phpmemcacheadmin* memcached-admin
+	rm phpmemcachedadmin.tar.gz
+else
+	echo "phpMemcachedAdmin already installed."
+fi
