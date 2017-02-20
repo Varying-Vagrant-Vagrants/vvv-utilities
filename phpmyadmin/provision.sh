9badcc3bb80ec627613aa8ab0dda4b2203168559
Lorelei Aurora <iam@loreleiaurora.me>
2017-02-20 14:08:10 +0000

    Update phpMyAdmin to 4.6.6

--- phpmyadmin/provision.sh
+++ phpmyadmin/provision.sh
@@ -6,9 +6,9 @@ DIR=`dirname $0`
 if [[ ! -d /srv/www/default/database-admin ]]; then
     echo "Downloading phpMyAdmin..."
     cd /srv/www/default
-    wget -q -O phpmyadmin.tar.gz "https://files.phpmyadmin.net/phpMyAdmin/4.6.0/phpMyAdmin-4.6.0-all-languages.tar.gz"
+    wget -q -O phpmyadmin.tar.gz "https://files.phpmyadmin.net/phpMyAdmin/4.6.6/phpMyAdmin-4.6.6-all-languages.tar.gz"
     tar -xf phpmyadmin.tar.gz
-    mv phpMyAdmin-4.6.0-all-languages database-admin
+    mv phpMyAdmin-4.6.6-all-languages database-admin
     rm phpmyadmin.tar.gz
 else
     echo "PHPMyAdmin already installed."
