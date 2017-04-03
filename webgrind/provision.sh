9c0a87ea868abe5acf8bbc43208b5075da3e3cbc
Christian Foellmann <foellmann@foe-services.de>
2017-04-03 20:31:23 +0200

    provision webgrind via composer

--- webgrind/provision.sh
+++ webgrind/provision.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 # Webgrind install (for viewing callgrind/cachegrind files produced by
 # xdebug profiler)
-if [[ ! -d "/srv/www/default/webgrind" ]]; then
-    echo -e "\nDownloading webgrind, see https://github.com/jokkedk/webgrind.git"
-    git clone "https://github.com/jokkedk/webgrind.git" "/srv/www/default/webgrind"
-else
-    echo -e "\nUpdating webgrind..."
-    cd /srv/www/default/webgrind
-    git pull --rebase origin master
-fi
+DIR=`dirname $0`
+
+# phpMyAdmin
+echo -e "Install/Update webgrind, see https://github.com/jokkedk/webgrind ..."
+cd "${DIR}"
+composer update --no-autoloader
