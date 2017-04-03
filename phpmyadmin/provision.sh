c1d3dc1f48361697b2eca6479096bd905b37ea86
Christian Foellmann <foellmann@foe-services.de>
2017-04-03 20:08:11 +0200

    provision phpmyadmin via composer

--- phpmyadmin/provision.sh
+++ phpmyadmin/provision.sh
@@ -2,15 +2,9 @@
 
 DIR=`dirname $0`
 
-# Download phpMyAdmin
-if [[ ! -d /srv/www/default/database-admin ]]; then
-    echo "Downloading phpMyAdmin..."
-    cd /srv/www/default
-    wget -q -O phpmyadmin.tar.gz "https://files.phpmyadmin.net/phpMyAdmin/4.7.0/phpMyAdmin-4.7.0-all-languages.tar.gz"
-    tar -xf phpmyadmin.tar.gz
-    mv phpMyAdmin-4.7.0-all-languages database-admin
-    rm phpmyadmin.tar.gz
-else
-    echo "PHPMyAdmin already installed."
-fi
+# phpMyAdmin
+echo -e "Install/Update phpMyAdmin..."
+cd "${DIR}"
+composer update --no-autoloader
+
 cp "${DIR}/config.inc.php" "/srv/www/default/database-admin/"
