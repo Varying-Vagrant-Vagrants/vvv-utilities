5487b83c3e8a4ee7347cccb0fe8aea94f18c5268
Tom J Nowell <contact@tomjn.com>
2018-05-31 21:12:49 +0100

    check for non composer installs and use the phpmyadmin repo

--- phpmyadmin/provision.sh
+++ phpmyadmin/provision.sh
@@ -3,8 +3,21 @@
 DIR=`dirname $0`
 
 # phpMyAdmin
-echo -e "Installing/Updating phpMyAdmin..."
-cd "${DIR}"
-composer update --no-autoloader
+mkdir -p /srv/www/default/database-admin
+cd "/srv/www/default/database-admin"
+if composer status; then
+  echo "Updating PHPMyAdmin"
+  composer update --no-autoloader
+else
+  echo "Installing PHPMyAdmin"
+  # clean up any old installs from pre-composer setup
+  rm -rf /srv/www/default/database-admin/
+  # Install using the phpmyadmin composer repo so that languages are bundled
+  composer create-project phpmyadmin/phpmyadmin --repository-url=https://www.phpmyadmin.net/packages.json --no-dev .
+end
+
+cd $DIR
+
+echo "Copying over PHPMyAdmin config file"
 
 cp "${DIR}/config.inc.php" "/srv/www/default/database-admin/"
