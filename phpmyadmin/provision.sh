0bd8ba8297c328b1fa2d145edb191a0418e222f3
Tom J Nowell <contact@tomjn.com>
2018-06-01 22:25:50 +0100

    Always replace PHPMyAdmin
    More reliable than the checks from previous commits

--- phpmyadmin/provision.sh
+++ phpmyadmin/provision.sh
@@ -3,18 +3,13 @@
 DIR=`dirname $0`
 
 # phpMyAdmin
+echo "Installing/Updating PHPMyAdmin"
+# clean up any old installs from pre-composer setup
+rm -rf /srv/www/default/database-admin/
 mkdir -p /srv/www/default/database-admin
 cd "/srv/www/default/database-admin"
-if composer status; then
-  echo "Updating PHPMyAdmin"
-  composer update --no-autoloader
-else
-  echo "Installing PHPMyAdmin"
-  # clean up any old installs from pre-composer setup
-  rm -rf /srv/www/default/database-admin/
-  # Install using the phpmyadmin composer repo so that languages are bundled
-  composer create-project phpmyadmin/phpmyadmin --repository-url=https://www.phpmyadmin.net/packages.json --no-dev .
-end
+# Install using the phpmyadmin composer repo so that languages are bundled
+composer create-project phpmyadmin/phpmyadmin --repository-url=https://www.phpmyadmin.net/packages.json --no-dev .
 
 cd $DIR
 
