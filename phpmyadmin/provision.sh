fafd6fa22b6664e8e107a478f23ff0cfd1f5a368
Tom J Nowell <contact@tomjn.com>
2018-07-04 15:34:09 +0100

    Update the phpmyadmin provisioner to use the tmp folder to be a little more flexible
    This should run without super user access now, also a little more informative, and switches to cp rather than mv

--- phpmyadmin/provision.sh
+++ phpmyadmin/provision.sh
@@ -2,16 +2,23 @@
 
 DIR=`dirname $0`
 
+echo "Checking phpMyAdmin"
+
 # Download phpMyAdmin
 if [[ ! -f /srv/www/default/database-admin/RELEASE-DATE-4.8.1 ]]; then
-    echo "Removing older phpMyAdmin install"
+    echo "Removing older phpMyAdmin install from /srv/www/default/database-admin"
     rm -rf /srv/www/default/database-admin/*
     echo "Downloading phpMyAdmin 4.8.1"
-    cd /srv/www/default
+    cd /tmp
     wget -q -O phpmyadmin.tar.gz "https://files.phpmyadmin.net/phpMyAdmin/4.8.1/phpMyAdmin-4.8.1-all-languages.tar.gz"
+    echo "Extracting phpMyAdmin 4.8.1 into /tmp"
     tar -xf phpmyadmin.tar.gz
-    mv phpMyAdmin-4.8.1-all-languages database-admin
-    rm phpmyadmin.tar.gz
+    echo "Copying phpMyAdmin into place"
+    cp -rf /tmp/phpMyAdmin-4.8.1-all-languages/* /srv/www/default/database-admin/*
+    echo "Cleaning up after phpMyAdmin"
+    rm -rf /tmp/phpMyAdmin-4.8.1-all-languages
+    rm -f /tmp/phpmyadmin.tar.gz
+    echo "phpMyAdmin setup complete"
 else
     echo "PHPMyAdmin already installed."
 fi
