bd116c93c7abf2b968d3cbc82f7bc97b89720185
Daniele Scasciafratte <mte90net@gmail.com>
2018-07-05 10:34:09 +0200

    added tideways

--- tideways/provision.sh
+++ tideways/provision.sh
@@ -5,7 +5,7 @@ if [[ ! -d "/srv/www/default/xhgui" ]]; then
     echo -e "\nDownloading xhgui, see https://github.com/perftools/xhgui"
     git clone "https://github.com/perftools/xhgui" "/srv/www/default/xhgui"
     chmod -r 775 /srv/www/default/xhgui/cache
-    apt-get -y install mongodb
+    sudo apt-get -y install mongodb
     # auto-remove records older than 2592000 seconds (30 days)
     mongo xhprof --eval 'db.collection.ensureIndex( { "meta.request_ts" : 1 }, { expireAfterSeconds : 2592000 } )'
 
@@ -15,9 +15,19 @@ if [[ ! -d "/srv/www/default/xhgui" ]]; then
     mongo xhprof --eval  "db.collection.ensureIndex( { 'profile.main().mu' : -1 } )"
     mongo xhprof --eval  "db.collection.ensureIndex( { 'profile.main().cpu' : -1 } )"
     mongo xhprof --eval  "db.collection.ensureIndex( { 'meta.url' : 1 } )"
-    # install tideways
     # edit nginx to add fastcgi_param PHP_VALUE "auto_prepend_file=/app/xhgui/external/header.php";
+    # install tideways
     # Tideways is only for php =>7.0
+    git clone "https://github.com/tideways/php-xhprof-extension" "/tmp/tideways"
+    phpize
+    ./configure
+    make
+    sudo make install
+    php -r 'echo ini_get("extension_dir");'
+    git clone "https://github.com/tideways/profiler/" "/tmp/tideways-profiler"
+    cd /tmp/tideways-profiler
+    sudo cp Tideways.php /usr/lib/php/20151012/
+    # restart nginx
 else
     echo -e "\nUpdating tideways..."
     cd /srv/www/default/xhgui
