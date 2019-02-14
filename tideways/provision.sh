ac0b52a73220da78a22193a5221efbc37fd90522
Daniele Scasciafratte <mte90net@gmail.com>
2019-02-14 17:44:50 +0100

    moved mongodb as utility

--- tideways/provision.sh
+++ tideways/provision.sh
@@ -48,36 +48,6 @@ restart_php() {
     service nginx restart > /dev/null 2>&1
 }
 
-install_mongodb() {
-    if [[ ! $(command -v mongo) ]]; then
-        echo "Install MongoDB"
-        apt-key add "${DIR}/aptkey.pgp"
-        echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-        sudo apt update > /dev/null 2>&1
-        apt-get -y install mongodb-org re2c > /dev/null 2>&1
-        for version in "7.0" "7.1" "7.2" "7.3"
-        do
-            if [[ $(command -v php$version) ]]; then
-                echo "Install MongoDB for PHP $version"
-                sudo pecl -d php_suffix="$version" install mongodb > /dev/null 2>&1
-                # do not remove files, only register the packages as not installed so we can install for other php version
-                sudo pecl uninstall -r mongodb > /dev/null 2>&1
-                cp -f "${DIR}/mongodb.ini" "/etc/php/$version/mods-available/mongodb.ini"
-                phpenmod -v "$version" mongodb
-            fi
-        done
-        # auto-remove records older than 2592000 seconds (30 days)
-        mongo xhprof --eval 'db.collection.ensureIndex( { "meta.request_ts" : 1 }, { expireAfterSeconds : 2592000 } )' > /dev/null 2>&1
-        # indexes
-        mongo xhprof --eval  "db.collection.ensureIndex( { 'meta.SERVER.REQUEST_TIME' : -1 } )" > /dev/null 2>&1
-        mongo xhprof --eval  "db.collection.ensureIndex( { 'profile.main().wt' : -1 } )" > /dev/null 2>&1
-        mongo xhprof --eval  "db.collection.ensureIndex( { 'profile.main().mu' : -1 } )" > /dev/null 2>&1
-        mongo xhprof --eval  "db.collection.ensureIndex( { 'profile.main().cpu' : -1 } )" > /dev/null 2>&1
-        mongo xhprof --eval  "db.collection.ensureIndex( { 'meta.url' : 1 } )" > /dev/null 2>&1
-        service mongod restart
-    fi
-}
-
 install_xhgui() {
     if [[ ! -d "/srv/www/default/xhgui" ]]; then
         echo -e "\nDownloading xhgui, see https://github.com/perftools/xhgui"
@@ -98,7 +68,6 @@ install_xhgui() {
 }
 
 echo "Installing Tideways & XHgui"
-install_mongodb
 install_tideways
 install_tideways_php
 install_xhgui
