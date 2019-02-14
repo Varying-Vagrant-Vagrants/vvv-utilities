949330e9cd18fcb25594c9b9d5c0f26fb65769a4
Daniele Scasciafratte <mte90net@gmail.com>
2019-02-14 14:31:42 +0100

    cleaning

--- tideways/provision.sh
+++ tideways/provision.sh
@@ -25,7 +25,7 @@ install_tideways_php() {
         update-alternatives --set php "/usr/bin/php$version" > /dev/null 2>&1
         update-alternatives --set php-config "/usr/bin/php-config$version" > /dev/null 2>&1
         update-alternatives --set phpize "/usr/bin/phpize$version" > /dev/null 2>&1
-        phpize$version
+        phpize$version > /dev/null 2>&1
         ./configure --enable-tideways-xhprof --with-php-config=php-config$version > /dev/null 2>&1
         make > /dev/null 2>&1
         make install > /dev/null 2>&1
@@ -33,6 +33,7 @@ install_tideways_php() {
 }
 
 restart_php() {
+    echo "Restarting PHP-FPM server"
     for version in 7.0 7.1 7.2 7.3
     do
         if [[ $(command -v php$version) ]]; then
@@ -43,6 +44,7 @@ restart_php() {
 }
 
 install_mongodb() {
+    echo "Install MongoDB"
     apt-key add "${DIR}/aptkey.pgp"
     echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
     sudo apt update > /dev/null 2>&1
@@ -50,7 +52,8 @@ install_mongodb() {
     for version in 7.0 7.1 7.2 7.3
     do
         if [[ $(command -v php$version) ]]; then
-            sudo pecl -d php_suffix=$version install mongodb
+            echo "Install MongoDB for PHP $version"
+            sudo pecl -d php_suffix="$version" install mongodb > /dev/null 2>&1
             cp -f "${DIR}/mongodb.ini" "/etc/php/$version/mods-available/mongodb.ini"
         fi
     done
@@ -63,7 +66,6 @@ install_mongodb() {
     mongo xhprof --eval  "db.collection.ensureIndex( { 'profile.main().mu' : -1 } )" > /dev/null 2>&1
     mongo xhprof --eval  "db.collection.ensureIndex( { 'profile.main().cpu' : -1 } )" > /dev/null 2>&1
     mongo xhprof --eval  "db.collection.ensureIndex( { 'meta.url' : 1 } )" > /dev/null 2>&1
-    update-rc.d mongodb defaults
     service mongod restart
 }
 
@@ -75,6 +77,7 @@ install_tideways
 for version in 7.0 7.1 7.2 7.3
 do
     if [[ $(command -v php$version) ]]; then
+        echo "Install Tideways for PHP $version"
         install_tideways_php "$version"
         phpenmod -v "$version" tideways_xhprof
     fi
@@ -84,18 +87,13 @@ if [[ ! -d "/srv/www/default/xhgui" ]]; then
     echo -e "\nDownloading xhgui, see https://github.com/perftools/xhgui"
     git clone "https://github.com/perftools/xhgui" "/srv/www/default/xhgui"
     cd /srv/www/default/xhgui
-    sudo php install.php > /dev/null 2>&1
+    echo "Installing Tideways"
+    sudo php install.php
     cp -f "${DIR}/config.php" "/srv/www/default/xhgui/config/config.php"
     cp -f "${DIR}/tideways-header.php" "/srv/www/default/xhgui/config/tideways-header.php"
     cp -f "${DIR}/nginx.conf" "/etc/nginx/custom-utilities/xhgui.conf"
     restart_php
     service mongod restart
-    for version in 7.0 7.1 7.2 7.3
-    do
-        if [[ $(command -v php$version) ]]; then
-            php$version --ri tideways_xhprof
-        fi
-    done
 else
     echo -e "\nUpdating xhgui..."
     cd /srv/www/default/xhgui
@@ -118,3 +116,4 @@ else
     restart_php
 fi
 
+echo "Finish installation of Tideways with xhgui"
