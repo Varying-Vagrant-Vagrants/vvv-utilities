c274b12c1c0f6c7132fe1f3f162cfa09f5c5d72d
Daniele Scasciafratte <mte90net@gmail.com>
2019-03-19 15:49:29 +0100

    migrate to package

--- tideways/provision.sh
+++ tideways/provision.sh
@@ -3,38 +3,12 @@
 DIR=$(dirname "$0")
 
 install_tideways() {
-    # Tideways is only for php =>7.0
-    if [[ ! -d /var/local/tideways-php/.git ]]; then
-        echo "Cloning Tideways extension"
-        git clone "https://github.com/tideways/php-xhprof-extension" /var/local/tideways-php
-    else
-        echo "Updating Tideways extension"
-        ( cd /var/local/tideways-php/ && git pull -q && git checkout -q )
+    if [[ ! -f '/etc/init.d/tideways-daemon' ]]; then
+        apt-key add "${DIR}/aptkey.pgp"
+        echo 'deb http://s3-eu-west-1.amazonaws.com/tideways/packages debian main' | sudo tee /etc/apt/sources.list.d/tideways.list
+        sudo apt update > /dev/null 2>&1
+        apt-get -y install tideways-php tideways-daemon > /dev/null 2>&1
     fi
-}
-
-install_tideways_php() {
-    echo "Installing Tideways for PHP $version"
-    for version in "7.0" "7.1" "7.2" "7.3"
-    do
-        if [[ $(command -v php$version) ]]; then
-            echo "Copying tideways files for PHP $version"
-            cp -f "${DIR}/tideways.ini" "/etc/php/$version/mods-available/tideways_xhprof.ini"
-            cp -f "${DIR}/xhgui-php.ini" "/etc/php/$version/mods-available/xhgui.ini"
-            cp -rf /var/local/tideways-php "/var/local/tideways-php$version"
-            echo "Compiling Tideways for PHP $version"
-            cd "/var/local/tideways-php${version}"
-            update-alternatives --set php "/usr/bin/php$version" > /dev/null 2>&1
-            update-alternatives --set php-config "/usr/bin/php-config$version" > /dev/null 2>&1
-            update-alternatives --set phpize "/usr/bin/phpize$version" > /dev/null 2>&1
-            phpize$version > /dev/null 2>&1
-            ./configure --enable-tideways-xhprof --with-php-config=php-config$version > /dev/null 2>&1
-            make > /dev/null 2>&1
-            make install > /dev/null 2>&1        
-            phpenmod -v "$version" tideways_xhprof
-            rm -rf "/var/local/tideways-php$version"
-        fi
-    done
 }
 
 restart_php() {
@@ -75,7 +49,6 @@ if [[ $(command -v mongo) ]]; then
     . "${DIR}/../mongodb/provision.sh"
 fi
 install_tideways
-install_tideways_php
 install_xhgui
 cp -f "${DIR}/nginx.conf" "/etc/nginx/custom-utilities/xhgui.conf"
 restart_php
