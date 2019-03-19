083036df2dfe07c6c6bae24a8a72a47ebcd38e11
Daniele Scasciafratte <mte90net@gmail.com>
2019-03-19 17:42:04 +0100

    avoid autoscan of xhgui

--- tideways/tideways-header.php
+++ tideways/tideways-header.php
@@ -1,6 +1,6 @@
 <?php
 
-if(strpos($_SERVER['REQUEST_URI'], 'enable-tideways') !== false) {
+if(strpos($_SERVER['REQUEST_URI'], 'vvv.test') === false && strpos($_SERVER['REQUEST_URI'], 'enable-tideways') !== false) {
     include '/srv/www/default/xhgui/external/header.php';
 }
 
