520fa645e2bfa573f24073a34898bd4056516a5e
Navjot Singh <navjotjsingh@gmail.com>
2019-03-20 12:09:55 +0530

    Update phpMyAdmin to 4.8.5
    Changes: https://www.phpmyadmin.net/news/

--- phpmyadmin/provision.sh
+++ phpmyadmin/provision.sh
@@ -5,19 +5,19 @@ DIR=`dirname $0`
 echo "Checking phpMyAdmin"
 
 # Download phpMyAdmin
-if [[ ! -f /srv/www/default/database-admin/RELEASE-DATE-4.8.1 ]]; then
+if [[ ! -f /srv/www/default/database-admin/RELEASE-DATE-4.8.5 ]]; then
     echo "Removing older phpMyAdmin install from /srv/www/default/database-admin"
     rm -rf /srv/www/default/database-admin/*
-    echo "Downloading phpMyAdmin 4.8.1"
+    echo "Downloading phpMyAdmin 4.8.5"
     cd /tmp
-    wget -q -O phpmyadmin.tar.gz "https://files.phpmyadmin.net/phpMyAdmin/4.8.1/phpMyAdmin-4.8.1-all-languages.tar.gz"
-    echo "Extracting phpMyAdmin 4.8.1 into /tmp"
+    wget -q -O phpmyadmin.tar.gz "https://files.phpmyadmin.net/phpMyAdmin/4.8.5/phpMyAdmin-4.8.5-all-languages.tar.gz"
+    echo "Extracting phpMyAdmin 4.8.5 into /tmp"
     tar -xf phpmyadmin.tar.gz
     echo "Copying phpMyAdmin into place"
     mkdir -p /srv/www/default/database-admin
-    cp -rf /tmp/phpMyAdmin-4.8.1-all-languages/* /srv/www/default/database-admin/
+    cp -rf /tmp/phpMyAdmin-4.8.5-all-languages/* /srv/www/default/database-admin/
     echo "Cleaning up after phpMyAdmin"
-    rm -rf /tmp/phpMyAdmin-4.8.1-all-languages
+    rm -rf /tmp/phpMyAdmin-4.8.5-all-languages
     rm -f /tmp/phpmyadmin.tar.gz
     echo "phpMyAdmin setup complete"
 else
