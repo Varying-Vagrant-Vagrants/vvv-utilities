0d99b135104ba1a4a7f5a1d90458944024c9cafa
Daniele Scasciafratte <mte90net@gmail.com>
2019-04-24 18:18:07 +0200

    code review

--- tideways/provision.sh
+++ tideways/provision.sh
@@ -78,20 +78,20 @@ enable_tideways_by_site() {
     rm /srv/tideways.json
 
     domains=""
-    input=$(cat ${VVV_CONFIG})
+    input=$(cat "${VVV_CONFIG}")
     sites=$(echo "$input" | shyaml keys-0 sites | xargs -0 -n 1 echo "")
 
     for value in ${sites}; do
-        domain=$(echo $value | tr -d '[:space:]')
-        tideways=$(echo "$input" | shyaml -q get-value sites."$value".tideways)
+        domain=$(echo "$value" | tr -d '[:space:]')
+        tideways=$(echo "$input" | shyaml -q get-value sites."$domain".tideways)
         if [[ $tideways != "" ]]; then
-            hosts=$(echo "$input" | shyaml -q get-value sites."$value".hosts)
+            hosts=$(echo "$input" | shyaml -q get-value sites."$domain".hosts)
             domains="$domains${hosts/\- /''}\n"
         fi
     done
     SAVEIFS=$IFS   # Save current IFS
     IFS=$'\n'      # Change IFS to new line
-    domains=($domains)
+    domains=("$domains")
     IFS=$SAVEIFS   # Restore IFS
     printf "${domains[@]}" | jq -R . | jq -s . > /srv/tideways.json
 }
