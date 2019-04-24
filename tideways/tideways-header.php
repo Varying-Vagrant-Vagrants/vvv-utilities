6b5dfa565ed9452b6a0b780f5b80d72d7a616ac4
Daniele Scasciafratte <mte90net@gmail.com>
2019-04-24 18:57:55 +0200

    support for json file in tideways

--- tideways/tideways-header.php
+++ tideways/tideways-header.php
@@ -1,6 +1,6 @@
 <?php
 
-if( isset( $_SERVER['REQUEST_URI'] ) && strpos( $_SERVER['REQUEST_URI'], 'enable-tideways' ) !== false ) {
+if( file_exists( '/srv/tideways.json' ) && in_array( $_SERVER['HTTP_HOST'], json_decode( file_get_contents( '/srv/tideways.json' ) ) ) || isset( $_SERVER['REQUEST_URI'] ) && strpos( $_SERVER['REQUEST_URI'], 'enable-tideways' ) !== false ) {
     if ( file_exists( '/srv/www/default/xhgui/external/header.php' ) ) {
         include '/srv/www/default/xhgui/external/header.php';
     }
