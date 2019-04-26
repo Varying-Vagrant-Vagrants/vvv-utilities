77ecfcd24787639b39728dd3765985c0c579dca8
Daniele Scasciafratte <mte90net@gmail.com>
2019-04-26 12:21:14 +0200

    better code in php

--- tideways/tideways-header.php
+++ tideways/tideways-header.php
@@ -1,6 +1,15 @@
 <?php
 
-if( file_exists( '/srv/tideways.json' ) && in_array( $_SERVER['HTTP_HOST'], json_decode( file_get_contents( '/srv/tideways.json' ) ) ) || isset( $_SERVER['REQUEST_URI'] ) && strpos( $_SERVER['REQUEST_URI'], 'enable-tideways' ) !== false ) {
+$is_vvv_tideways = false;
+if ( file_exists( '/srv/tideways.json' ) && in_array( $_SERVER['HTTP_HOST'], json_decode( file_get_contents( '/srv/tideways.json' ) ) ) ) {
+    $is_vvv_tideways = true;
+}
+    
+if ( isset( $_SERVER['REQUEST_URI'] ) && strpos( $_SERVER['REQUEST_URI'], 'enable-tideways' ) !== false ) {
+    $is_vvv_tideways = true;
+}
+
+if( $is_vvv_tideways ) {
     if ( file_exists( '/srv/www/default/xhgui/external/header.php' ) ) {
         include '/srv/www/default/xhgui/external/header.php';
     }
