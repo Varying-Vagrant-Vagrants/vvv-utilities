47deab944372dc54dcf81c71098a052cf17fdc7b
Tom J Nowell <contact@tomjn.com>
2019-08-06 21:42:20 +0100

    add a http header when tideways is enabled, and adjust the conditional
    Needs testing

--- tideways/tideways-header.php
+++ tideways/tideways-header.php
@@ -1,12 +1,13 @@
 <?php
 
-if ( !empty( $_REQUEST['enable-tideways'] ) && ( $_REQUEST['enable-tideways'] == true ) ) {
+if ( !empty( $_REQUEST['enable-tideways'] ) && ( $_REQUEST['enable-tideways'] != false ) ) {
 
     if ( file_exists( '/srv/www/default/xhgui/external/header.php' ) ) {
         include '/srv/www/default/xhgui/external/header.php';
     }
 
     define( 'QM_DISABLED', true );
+    header( 'vvv-php-tideways: active');
     if( file_exists( './custom-header.php' ) ) {
         include_once( './custom-header.php' );
     }
