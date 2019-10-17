09983d1071c41875a403c66a8e4b8dcd05b01402
tomjn <contact@tomjn.com>
2019-10-17 22:24:34 +0100

    fix script execution of the PHP provisioners

--- php/provision.sh
+++ php/provision.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 echo " * Running all PHP provisioners"
 
-(cd ../php56 && source provision.sh )
-(cd ../php70 && source provision.sh )
-(cd ../php71 && source provision.sh )
-(cd ../php72 && source provision.sh )
-(cd ../php73 && source provision.sh )
-(cd ../php74 && source provision.sh )
+( cd "${DIR}/../php56/" && . provision.sh )
+( cd "${DIR}/../php70/" && . provision.sh )
+( cd "${DIR}/../php71/" && . provision.sh )
+( cd "${DIR}/../php72/" && . provision.sh )
+( cd "${DIR}/../php73/" && . provision.sh )
+( cd "${DIR}/../php74/" && . provision.sh )
 
 echo " * Finished running PHP provisioners"
