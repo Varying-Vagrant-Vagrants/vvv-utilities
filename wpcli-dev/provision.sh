102a6317e75457538399aa1757d71095da969904
Daniele Scasciafratte <mte90net@gmail.com>
2019-10-18 15:20:51 +0200

    More statements and check for updates

--- wpcli-dev/provision.sh
+++ wpcli-dev/provision.sh
@@ -17,11 +17,21 @@ mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
 mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO wp_cli_test@localhost IDENTIFIED BY 'password1';"
 echo -e " DB operations done.\n"
 
-sudo apt install -y jq
+echo "Downloading jq utility"
+apt install -y jq
 
-noroot git clone https://github.com/wp-cli/wp-cli-dev
+if [[ ! -d /srv/www/wp-cli-dev/.git ]]; then
+  echo "Downloading official WP-CLI-Dev environment"
+  noroot git clone https://github.com/wp-cli/wp-cli-dev
+else
+  echo "Updating official WP-CLI-Dev environment"
+  ( cd /srv/www/wp-cli-dev/ && noroot git pull -q && noroot git checkout -q )
+fi
+
 cd wp-cli-dev
 noroot composer install --no-dev
 
 echo "Creating the symlink as wp-dev"
 ln -sf /srv/www/wp-cli-dev/vendor/bin/wp /usr/local/bin/wp-dev
+
+echo "WP-CLI Dev provisioned"
