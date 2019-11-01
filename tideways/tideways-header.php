aac5b29b2d8a3760d6af84ff78a9172bf4df4dc0
Tom J Nowell <contact@tomjn.com>
2019-11-01 14:11:47 +0000

    Adjust Tideways loading (#40)

--- tideways/tideways-header.php
+++ tideways/tideways-header.php
@@ -1,25 +1,35 @@
 <?php
 
+// don't run on CLI
+if ( empty($_SERVER['REMOTE_ADDR']) and !isset($_SERVER['HTTP_USER_AGENT']) and count($_SERVER['argv']) > 0) {
+    return;
+} 
+
+if ( !isset( $_SERVER['HTTP_HOST'] ) ) {
+    return;
+}
+if ( strpos( $_SERVER['HTTP_HOST'], 'vvv.test' ) != false ) {
+    return;
+}
+
 $is_vvv_tideways = false;
-if ( isset( $_SERVER['HTTP_HOST'] ) && strpos( $_SERVER['HTTP_HOST'], 'vvv.test' ) == false ) {
-    
-    if ( file_exists( '/srv/config/tideways.json' ) && in_array( $_SERVER['HTTP_HOST'], json_decode( file_get_contents( '/srv/config/tideways.json' ) ) ) ) {
-        $is_vvv_tideways = true;
-    }
-        
-    if ( isset( $_REQUEST['enable-tideways'] ) && ( $_REQUEST['enable-tideways'] == true ) ) {
-        $is_vvv_tideways = true;
-    }
 
-    if( $is_vvv_tideways ) {
-        if ( file_exists( '/srv/www/default/xhgui/external/header.php' ) ) {
-            include '/srv/www/default/xhgui/external/header.php';
-        }
+if ( file_exists( '/srv/config/tideways.json' ) && in_array( $_SERVER['HTTP_HOST'], json_decode( file_get_contents( '/srv/config/tideways.json' ) ) ) ) {
+    $is_vvv_tideways = true;
+}
 
-        define( 'QM_DISABLED', true );
-        if( file_exists( '/srv/www/default/xhgui/config/custom-header.php' ) ) {
-            include_once( '/srv/www/default/xhgui/config/custom-header.php' );
-        }
-    }
+if ( isset( $_REQUEST['enable-tideways'] ) && ( $_REQUEST['enable-tideways'] == true ) ) {
+    $is_vvv_tideways = true;
+}
 
+if( ! $is_vvv_tideways ) {
+    return;
+}
+if ( file_exists( '/srv/www/default/xhgui/external/header.php' ) ) {
+    include '/srv/www/default/xhgui/external/header.php';
+}
+
+define( 'QM_DISABLED', true );
+if( file_exists( '/srv/www/default/xhgui/config/custom-header.php' ) ) {
+    include_once( '/srv/www/default/xhgui/config/custom-header.php' );
 }
