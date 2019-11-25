ef9fdace32b29ad7625426a89518fcb6a4a0d3e3
Tom J Nowell <contact@tomjn.com>
2019-11-25 15:18:05 +0000

    Update PHPMyAdmin (#41)
    * Update PHPMyAdmin

--- phpmyadmin/provision.sh
+++ phpmyadmin/provision.sh
@@ -2,25 +2,25 @@
 
 DIR=`dirname $0`
 
-echo "Checking phpMyAdmin"
+echo " * Checking for phpMyAdmin 4.9.1"
 
 # Download phpMyAdmin
-if [[ ! -f /srv/www/default/database-admin/RELEASE-DATE-4.8.5 ]]; then
+if [[ ! -f /srv/www/default/database-admin/RELEASE-DATE-4.9.1 ]]; then
     echo "Removing older phpMyAdmin install from /srv/www/default/database-admin"
     rm -rf /srv/www/default/database-admin/*
-    echo "Downloading phpMyAdmin 4.8.5"
+    echo "Downloading phpMyAdmin 4.9.1"
     cd /tmp
-    wget -q -O phpmyadmin.tar.gz "https://files.phpmyadmin.net/phpMyAdmin/4.8.5/phpMyAdmin-4.8.5-all-languages.tar.gz"
-    echo "Extracting phpMyAdmin 4.8.5 into /tmp"
-    tar -xf phpmyadmin.tar.gz
-    echo "Copying phpMyAdmin into place"
+    wget -q -O phpmyadmin.zip "https://files.phpmyadmin.net/phpMyAdmin/4.9.1/phpMyAdmin-4.9.1-all-languages.zip"
+    echo " *Extracting phpMyAdmin 4.9.1 into /tmp"
+    unzip phpmyadmin.zip
+    echo " * Copying phpMyAdmin into place"
     mkdir -p /srv/www/default/database-admin
-    cp -rf /tmp/phpMyAdmin-4.8.5-all-languages/* /srv/www/default/database-admin/
-    echo "Cleaning up after phpMyAdmin"
-    rm -rf /tmp/phpMyAdmin-4.8.5-all-languages
-    rm -f /tmp/phpmyadmin.tar.gz
-    echo "phpMyAdmin setup complete"
+    cp -rf /tmp/phpMyAdmin-4.9.1-all-languages/* /srv/www/default/database-admin/
+    echo " * Cleaning up after phpMyAdmin"
+    rm -rf /tmp/phpMyAdmin*
+    rm -f /tmp/phpmyadmin.zip
+    echo " * phpMyAdmin setup complete"
 else
-    echo "PHPMyAdmin already installed."
+    echo " * PHPMyAdmin 4.9.1 already installed."
 fi
 cp "${DIR}/config.inc.php" "/srv/www/default/database-admin/"
