e2514821e836b95950aa02cb55a88c6c1c53ae05
tomjn <contact@tomjn.com>
2019-12-03 17:43:07 +0000

    fix potential globbing in TLS CA and add the vvv domain to the main certificate

--- tls-ca/provision.sh
+++ tls-ca/provision.sh
@@ -14,21 +14,21 @@ fi
 
 CA_DIR="${CERTIFICATES_DIR}/ca"
 
-if [ ! -d ${CA_DIR} ];then
+if [ ! -d "${CA_DIR}" ];then
     echo " * Setting up VVV Certificate Authority"
-    mkdir -p ${CA_DIR}
+    mkdir -p "${CA_DIR}"
 
     openssl genrsa \
-        -out ${CA_DIR}/ca.key \
+        -out "${CA_DIR}/ca.key" \
         2048 &>/dev/null
 
     openssl req \
         -x509 -new \
         -nodes \
-        -key ${CA_DIR}/ca.key \
+        -key "${CA_DIR}/ca.key" \
         -sha256 \
         -days 3650 \
-        -out ${CA_DIR}/ca.crt \
+        -out "${CA_DIR}/ca.crt" \
         -subj "/CN=VVV INTERNAL CA" &>/dev/null
 fi
 
@@ -47,7 +47,7 @@ CERT_DIR="${CERTIFICATES_DIR}/default"
 rm -rf "${CERT_DIR}"
 mkdir -p "${CERT_DIR}"
 
-cat << EOF > ${CERT_DIR}/openssl.conf
+cat << EOF > "${CERT_DIR}/openssl.conf"
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
@@ -58,56 +58,58 @@ DNS.1 = vvv.test
 DNS.2 = *.vvv.test
 DNS.3 = vvv.local
 DNS.4 = *.vvv.local
+DNS.5 = vvv
+DNS.6 = *.vvv.test
 EOF
 
 openssl genrsa \
-    -out ${CERT_DIR}/dev.key \
+    -out $"{CERT_DIR}/dev.key" \
     2048 &>/dev/null
 
 openssl req \
     -new \
-    -key ${CERT_DIR}/dev.key \
-    -out ${CERT_DIR}/dev.csr \
+    -key "${CERT_DIR}/dev.key" \
+    -out "${CERT_DIR}/dev.csr" \
     -subj "/CN=vvv.test"  &>/dev/null
 
 openssl x509 \
     -req \
-    -in ${CERT_DIR}/dev.csr \
-    -CA ${CA_DIR}/ca.crt \
-    -CAkey ${CA_DIR}/ca.key \
+    -in "${CERT_DIR}/dev.csr" \
+    -CA "${CA_DIR}/ca.crt" \
+    -CAkey "${CA_DIR}/ca.key" \
     -CAcreateserial \
-    -out ${CERT_DIR}/dev.crt \
+    -out "${CERT_DIR}/dev.crt" \
     -days 3650 \
     -sha256 \
-    -extfile ${CERT_DIR}/openssl.conf  &>/dev/null
+    -extfile "${CERT_DIR}/openssl.conf"  &>/dev/null
 
 echo " * Symlinking default server certificate and key"
 
 rm -rf /etc/nginx/server-2.1.0.crt
 rm -rf /etc/nginx/server-2.1.0.key
 
-ln -s ${CERT_DIR}/dev.crt /etc/nginx/server-2.1.0.crt
-ln -s ${CERT_DIR}/dev.key /etc/nginx/server-2.1.0.key
+ln -s "${CERT_DIR}/dev.crt" /etc/nginx/server-2.1.0.crt
+ln -s "${CERT_DIR}/dev.key" /etc/nginx/server-2.1.0.key
 
 get_sites() {
-    local value=`cat ${VVV_CONFIG} | shyaml keys sites 2> /dev/null`
-    echo ${value:-$@}
+    local value=$(shyaml keys sites 2> /dev/null < ${VVV_CONFIG})
+    echo "${value:-$@}"
 }
 
 get_host() {
-    local value=`cat ${VVV_CONFIG} | shyaml get-value sites.${1}.hosts.0 2> /dev/null`
-    echo ${value:-$@}
+    local value=$(shyaml "get-value sites.${1}.hosts.0" 2> /dev/null < ${VVV_CONFIG})
+    echo "${value:-$@}"
 }
 
 get_hosts() {
-    local value=`cat ${VVV_CONFIG} | shyaml get-values sites.${1}.hosts 2> /dev/null`
-    echo ${value:-$@}
+    local value=$(shyaml "get-values sites.${1}.hosts" 2> /dev/null < ${VVV_CONFIG})
+    echo "${value:-$@}"
 }
 
 echo " * Generating Site certificates"
-for SITE in `get_sites`; do
-    echo "Generating certificates for the ${SITE} hosts"
-    SITE_ESCAPED=$(echo ${SITE} | sed 's/\./\\\\./g')
+for SITE in $(get_sites); do
+    echo " * Generating certificates for the '${SITE}'' hosts"
+    SITE_ESCAPED=$(echo "${SITE}" | sed 's/\./\\\\./g')
     COMMON_NAME=$(get_host "${SITE_ESCAPED}")
     HOSTS=$(get_hosts "${SITE_ESCAPED}")
     CERT_DIR="${CERTIFICATES_DIR}/${SITE}"
@@ -115,7 +117,7 @@ for SITE in `get_sites`; do
     rm -rf "${CERT_DIR}"
     mkdir -p "${CERT_DIR}"
 
-    cat << EOF > ${CERT_DIR}/openssl.conf
+    cat << EOF > "${CERT_DIR}/openssl.conf"
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
@@ -126,31 +128,31 @@ EOF
     I=0
     for DOMAIN in ${HOSTS}; do
         ((I++))
-        echo DNS.${I} = ${DOMAIN} >> ${CERT_DIR}/openssl.conf
+        echo "DNS.${I} = ${DOMAIN}" >> "${CERT_DIR}/openssl.conf"
         ((I++))
-        echo DNS.${I} = *.${DOMAIN} >> ${CERT_DIR}/openssl.conf
+        echo "DNS.${I} = *.${DOMAIN}" >> "${CERT_DIR}/openssl.conf"
     done
 
     openssl genrsa \
-        -out ${CERT_DIR}/dev.key \
+        -out "${CERT_DIR}/dev.key" \
         2048 &>/dev/null
 
     openssl req \
         -new \
-        -key ${CERT_DIR}/dev.key \
-        -out ${CERT_DIR}/dev.csr \
+        -key "${CERT_DIR}/dev.key" \
+        -out "${CERT_DIR}/dev.csr" \
         -subj "/CN=${COMMON_NAME}" &>/dev/null
 
     openssl x509 \
         -req \
-        -in ${CERT_DIR}/dev.csr \
-        -CA ${CA_DIR}/ca.crt \
-        -CAkey ${CA_DIR}/ca.key \
+        -in "${CERT_DIR}/dev.csr" \
+        -CA "${CA_DIR}/ca.crt" \
+        -CAkey "${CA_DIR}/ca.key" \
         -CAcreateserial \
-        -out ${CERT_DIR}/dev.crt \
+        -out "${CERT_DIR}/dev.crt" \
         -days 3650 \
         -sha256 \
-        -extfile ${CERT_DIR}/openssl.conf &>/dev/null
+        -extfile "${CERT_DIR}/openssl.conf" &>/dev/null
 done
 
 
