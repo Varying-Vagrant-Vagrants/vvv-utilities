ee93d71f323f4c31d33669946664621fa4808483
tomjn <contact@tomjn.com>
2019-12-03 19:00:40 +0000

    TLS CA fixes and improvements

--- tls-ca/provision.sh
+++ tls-ca/provision.sh
@@ -42,12 +42,12 @@ fi
 
 echo " * Setting up default Certificate for vvv.test and vvv.local"
 
-CERT_DIR="${CERTIFICATES_DIR}/default"
+DEFAULT_CERT_DIR="${CERTIFICATES_DIR}/default"
 
-rm -rf "${CERT_DIR}"
-mkdir -p "${CERT_DIR}"
+rm -rf "${DEFAULT_CERT_DIR}"
+mkdir -p "${DEFAULT_CERT_DIR}"
 
-cat << EOF > "${CERT_DIR}/openssl.conf"
+cat << EOF > "${DEFAULT_CERT_DIR}/openssl.conf"
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
@@ -63,33 +63,36 @@ DNS.6 = *.vvv.test
 EOF
 
 openssl genrsa \
-    -out $"{CERT_DIR}/dev.key" \
+    -out "${DEFAULT_CERT_DIR}/dev.key" \
     2048 &>/dev/null
 
 openssl req \
     -new \
-    -key "${CERT_DIR}/dev.key" \
-    -out "${CERT_DIR}/dev.csr" \
+    -key "${DEFAULT_CERT_DIR}/dev.key" \
+    -out "${DEFAULT_CERT_DIR}/dev.csr" \
     -subj "/CN=vvv.test"  &>/dev/null
 
 openssl x509 \
     -req \
-    -in "${CERT_DIR}/dev.csr" \
+    -in "${DEFAULT_CERT_DIR}/dev.csr" \
     -CA "${CA_DIR}/ca.crt" \
     -CAkey "${CA_DIR}/ca.key" \
     -CAcreateserial \
-    -out "${CERT_DIR}/dev.crt" \
+    -out "${DEFAULT_CERT_DIR}/dev.crt" \
     -days 3650 \
     -sha256 \
-    -extfile "${CERT_DIR}/openssl.conf"  &>/dev/null
+    -extfile "${DEFAULT_CERT_DIR}/openssl.conf"  &>/dev/null
 
 echo " * Symlinking default server certificate and key"
 
 rm -rf /etc/nginx/server-2.1.0.crt
 rm -rf /etc/nginx/server-2.1.0.key
 
-ln -s "${CERT_DIR}/dev.crt" /etc/nginx/server-2.1.0.crt
-ln -s "${CERT_DIR}/dev.key" /etc/nginx/server-2.1.0.key
+echo " * Symlinking ${DEFAULT_CERT_DIR}/dev.crt to /etc/nginx/server-2.1.0.crt"
+ln -s "${DEFAULT_CERT_DIR}/dev.crt" /etc/nginx/server-2.1.0.crt
+
+echo " * Symlinking ${DEFAULT_CERT_DIR}/dev.key to /etc/nginx/server-2.1.0.key"
+ln -s "${DEFAULT_CERT_DIR}/dev.key" /etc/nginx/server-2.1.0.key
 
 get_sites() {
     local value=$(shyaml keys sites 2> /dev/null < ${VVV_CONFIG})
@@ -108,16 +111,16 @@ get_hosts() {
 
 echo " * Generating Site certificates"
 for SITE in $(get_sites); do
-    echo " * Generating certificates for the '${SITE}'' hosts"
+    echo " * Generating certificates for the '${SITE}' hosts"
     SITE_ESCAPED=$(echo "${SITE}" | sed 's/\./\\\\./g')
     COMMON_NAME=$(get_host "${SITE_ESCAPED}")
     HOSTS=$(get_hosts "${SITE_ESCAPED}")
-    CERT_DIR="${CERTIFICATES_DIR}/${SITE}"
+    SITE_CERT_DIR="${CERTIFICATES_DIR}/${SITE}"
 
-    rm -rf "${CERT_DIR}"
-    mkdir -p "${CERT_DIR}"
+    rm -rf "${SITE_CERT_DIR}"
+    mkdir -p "${SITE_CERT_DIR}"
 
-    cat << EOF > "${CERT_DIR}/openssl.conf"
+    cat << EOF > "${SITE_CERT_DIR}/openssl.conf"
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
@@ -128,31 +131,31 @@ EOF
     I=0
     for DOMAIN in ${HOSTS}; do
         ((I++))
-        echo "DNS.${I} = ${DOMAIN}" >> "${CERT_DIR}/openssl.conf"
+        echo "DNS.${I} = ${DOMAIN}" >> "${SITE_CERT_DIR}/openssl.conf"
         ((I++))
-        echo "DNS.${I} = *.${DOMAIN}" >> "${CERT_DIR}/openssl.conf"
+        echo "DNS.${I} = *.${DOMAIN}" >> "${SITE_CERT_DIR}/openssl.conf"
     done
 
     openssl genrsa \
-        -out "${CERT_DIR}/dev.key" \
+        -out "${SITE_CERT_DIR}/dev.key" \
         2048 &>/dev/null
 
     openssl req \
         -new \
-        -key "${CERT_DIR}/dev.key" \
-        -out "${CERT_DIR}/dev.csr" \
+        -key "${SITE_CERT_DIR}/dev.key" \
+        -out "${SITE_CERT_DIR}/dev.csr" \
         -subj "/CN=${COMMON_NAME}" &>/dev/null
 
     openssl x509 \
         -req \
-        -in "${CERT_DIR}/dev.csr" \
+        -in "${SITE_CERT_DIR}/dev.csr" \
         -CA "${CA_DIR}/ca.crt" \
         -CAkey "${CA_DIR}/ca.key" \
         -CAcreateserial \
-        -out "${CERT_DIR}/dev.crt" \
+        -out "${SITE_CERT_DIR}/dev.crt" \
         -days 3650 \
         -sha256 \
-        -extfile "${CERT_DIR}/openssl.conf" &>/dev/null
+        -extfile "${SITE_CERT_DIR}/openssl.conf" &>/dev/null
 done
 
 
