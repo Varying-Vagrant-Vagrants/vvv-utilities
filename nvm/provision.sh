6760501a5ce0e004e070ec7dc60829ffc31e7650
Amit Gupta <coolamit@gmail.com>
2020-06-09 05:27:52 +0530

    Added nvm provisioner as an optional core utility for VVV

---
+++ nvm/provision.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+# NVM install to facilitate multiple versions of Nodejs
+# Makes it easier to install new versions of Nodejs and
+# to work with multiple projects that need different
+# versions of Nodejs
+
+echo " * Checking for NVM"
+
+if [[ -d ~/.nvm && -f ~/.nvm/nvm.sh ]]
+then
+  echo " * NVM is already installed, no need to install again"
+else
+  if [[ -d ~/.nvm && ! -f ~/.nvm/nvm.sh ]]
+  then
+    # Possible remnants or something messed
+    # up NVM install making it unusable
+    echo " * NVM found in an unusable state, removing it completely"
+    rm -rf ~/.nvm
+  fi
+
+  # lets install it now
+  echo " * NVM installation starting now"
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+  echo " * NVM installed"
+
+  # set default node to the one installed by VVV
+  nvm alias default system
+  echo " * NVM default set to system default"
+fi
