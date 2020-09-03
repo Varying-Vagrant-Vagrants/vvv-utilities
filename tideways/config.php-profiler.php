0cf070e84b2a98a19b99d2666a6a2aa48fc2c377
Daniele Scasciafratte <mte90net@gmail.com>
2020-09-03 16:27:32 +0200

    added default settings

--- tideways/config.php-profiler.php
+++ tideways/config.php-profiler.php
@@ -3,23 +3,22 @@
 require_once 'vendor/autoload.php';
 
 $config = array(
-    // If defined, use specific profiler
-    // otherwise use any profiler that's found
-    'profiler' => \Xhgui\Profiler\Profiler::PROFILER_TIDEWAYS_XHPROF,
-
-    // This allows to configure, what profiling data to capture
-    'profiler.flags' => array(
-        \Xhgui\Profiler\ProfilingFlags::CPU,
-        \Xhgui\Profiler\ProfilingFlags::MEMORY,
-        \Xhgui\Profiler\ProfilingFlags::NO_BUILTINS,
-        \Xhgui\Profiler\ProfilingFlags::NO_SPANS,
-    ),
     'save.handler' => \Xhgui\Profiler\Profiler::SAVER_PDO,
     'save.handler.pdo' => array(
         'dsn' => 'sqlite:/tmp/xhgui.sqlite3',
         'user' => null,
         'pass' => null,
         'table' => 'results'
+    ),
+    // If defined, use specific profiler
+    // otherwise use any profiler that's found
+    'profiler' => \Xhgui\Profiler\Profiler::PROFILER_TIDEWAYS_XHPROF,
+    // This allows to configure, what profiling data to capture
+    'profiler.flags' => array(
+        \Xhgui\Profiler\ProfilingFlags::CPU,
+        \Xhgui\Profiler\ProfilingFlags::MEMORY,
+        \Xhgui\Profiler\ProfilingFlags::NO_BUILTINS,
+        \Xhgui\Profiler\ProfilingFlags::NO_SPANS,
     ),
     // Environment variables to exclude from profiling data
     'profiler.exclude-env' => array(
@@ -37,7 +36,13 @@ $config = array(
         $uri = str_replace('?enable-tideways', '', $uri);
         $uri = str_replace('%3Fenable-tideways', '', $uri);
         return $uri;
-    }
+    },
+    'profiler.simple_url' => function($url) {
+        return $url;
+    },
+    'profiler.options' => array(),
+    'profiler.skip_built_in' => false,
+    'fastcgi_finish_request' => true,
 ); 
 
 $custom_args = array();
