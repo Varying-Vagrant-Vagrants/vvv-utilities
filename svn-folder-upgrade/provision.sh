8374cf96aa671aa2579084293d1e46d23617f8c6
Tom J Nowell <contact@tomjn.com>
2020-09-04 18:59:03 +0100

    Add an svn folder upgrade utility

---
+++ svn-folder-upgrade/provision.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo " * Running all PHP provisioners"
+
+wpsvn_check() {
+  # Get all SVN repos.
+  svn_repos=$(find /srv/www -maxdepth 5 -type d -name '.svn');
+
+  # Do we have any?
+  if [[ -n $svn_repos ]]; then
+    echo " * ${#svn_repos[@]} SVN repositories found, checking for upgrades..."
+    for repo in $svn_repos; do
+      # Test to see if an svn upgrade is needed on this repo.
+      svn_test=$( svn status -u "$repo" 2>&1 );
+
+      if [[ "$svn_test" == *"svn upgrade"* ]]; then
+        # If it is needed do it!
+        echo " * Upgrading SVN repository at: ${repo}"
+        svn upgrade "${repo/%\.svn/}"
+        echo " * SVN upgrade command finished in  ${repo}"
+      fi;
+    done
+  fi;
+}
+
+echo " * Searching for SVN repositories that need upgrading."
+wpsvn_check
+echo " * SVN repository upgrade search has ended."
