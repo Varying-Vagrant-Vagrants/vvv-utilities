ad12ba458ac76f3dae56374c21f315dfec1e02ed
Daniele Scasciafratte <mte90net@gmail.com>
2020-10-02 16:38:12 +0200

    just one cd

--- opcache-gui/provision.sh
+++ opcache-gui/provision.sh
@@ -4,12 +4,11 @@
 if [[ ! -d "/srv/www/default/opcache-gui" ]]; then
 	echo -e " * Downloading Opcache GUI, see https://github.com/amnuts/opcache-gui"
 	noroot mkdir -p /srv/www/default/opcache-gui
-	cd /srv/www/default/opcache-gui
 else
 	echo -e " * Updating Opcache GUI"
-	cd /srv/www/default/opcache-gui
 fi
 
+cd /srv/www/default/opcache-gui
 noroot composer require amnuts/opcache-gui
 
 noroot ln -s ./vendor/amnuts/opcache-gui/index.php ./index.php
