dc123ab053204a081fa403962f39cf13f5d2ba2a
Tom J Nowell <contact@tomjn.com>
2020-10-12 16:22:33 +0100

    Update provision.sh

--- opcache-gui/provision.sh
+++ opcache-gui/provision.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 # Checkout Opcache GUI to provide a dashboard for viewing statistics
 
-if [[ ! -d "/srv/www/default/opcache-gui" ]]; then
-	echo -e " * Downloading Opcache GUI, see https://github.com/amnuts/opcache-gui"
-	noroot mkdir -p /srv/www/default/opcache-gui
-else
-	echo -e " * Updating Opcache GUI"
-fi
-
+noroot mkdir -p /srv/www/default/opcache-gui
+echo -e " * Provisioning Opcache GUI"
 cd /srv/www/default/opcache-gui
 noroot composer require amnuts/opcache-gui
 
+echo " * symlinking index.php"
+
 noroot ln -s ./vendor/amnuts/opcache-gui/index.php ./index.php
+
+echo " * Opcache GUI complete"
