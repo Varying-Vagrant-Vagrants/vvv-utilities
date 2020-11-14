0bcccc2a61ec51c57b0c0d089367abaeca5ad717
Tom J Nowell <contact@tomjn.com>
2020-11-14 16:47:54 +0000

    update phpMyAdmin to v4.9.7

--- phpmyadmin/provision.sh
+++ phpmyadmin/provision.sh
@@ -2,25 +2,29 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo " * Checking for phpMyAdmin v4.9.1"
+PHPMYADMINVERSION="4.9.7"
+echo " * Checking for phpMyAdmin v${PHPMYADMINVERSION}"
 
 # Download phpMyAdmin
-if [[ ! -f /srv/www/default/database-admin/RELEASE-DATE-4.9.1 ]]; then
+if [[ ! -f "/srv/www/default/database-admin/RELEASE-DATE-${PHPMYADMINVERSION}" ]]; then
     echo " * Removing older phpMyAdmin install from /srv/www/default/database-admin"
     rm -rf /srv/www/default/database-admin/*
-    echo " * Downloading phpMyAdmin 4.9.1"
+    echo " * Downloading phpMyAdmin v${PHPMYADMINVERSION}"
     cd /tmp
-    wget -q -O phpmyadmin.zip "https://files.phpmyadmin.net/phpMyAdmin/4.9.1/phpMyAdmin-4.9.1-all-languages.zip"
-    echo " * Extracting phpMyAdmin v4.9.1 into /tmp"
+    wget -q -O phpmyadmin.zip "https://files.phpmyadmin.net/phpMyAdmin/${PHPMYADMINVERSION}/phpMyAdmin-${PHPMYADMINVERSION}-all-languages.zip"
+    echo " * Extracting phpMyAdmin v${PHPMYADMINVERSION} into /tmp"
     unzip phpmyadmin.zip
-    echo " * Copying phpMyAdmin into place"
-    mkdir -p /srv/www/default/database-admin
-    cp -rf /tmp/phpMyAdmin-4.9.1-all-languages/* /srv/www/default/database-admin/
-    echo " * Cleaning up after phpMyAdmin"
-    rm -rf /tmp/phpMyAdmin*
     rm -f /tmp/phpmyadmin.zip
-    echo " * phpMyAdmin setup complete"
+    mkdir -p /srv/www/default/database-admin
+    echo " * Clearing out old phpMyAdmin"
+    rm -rf /srv/www/default/database-admin/*
+    echo " * Copying phpMyAdmin v${PHPMYADMINVERSION} into place"
+    cp -rf /tmp/phpMyAdmin-${PHPMYADMINVERSION}-all-languages/* /srv/www/default/database-admin/
+    echo " * Cleaning up install files"
+    rm -rf /tmp/phpMyAdmin*
+    echo " * phpMyAdmin v${PHPMYADMINVERSION} installation complete"
 else
-    echo " * PHPMyAdmin v4.9.1 already installed."
+    echo " * PHPMyAdmin v${PHPMYADMINVERSION} already installed."
 fi
+echo " * Overwriting config file with the latest version"
 cp -f "${DIR}/config.inc.php" "/srv/www/default/database-admin/"
