d0b024ab2ed513a8cd0e74e25553e1ecbd85bdb9
Daniele Scasciafratte <mte90net@gmail.com>
2022-01-19 19:07:05 +0100

    fix last suggestions

--- tideways/config.php-profiler.php
+++ tideways/config.php-profiler.php
@@ -54,10 +54,11 @@ $config = array(
             }
             
             try { 
-                if ( file_exists( '/srv/config/tideways.json' ) && 
-                    in_array( $_SERVER['HTTP_HOST'], json_decode( file_get_contents( '/srv/config/tideways.json' ) ) ) 
-                   ) {
-                    $is_vvv_tideways = true;
+                if ( file_exists( '/srv/config/tideways.json' ) ) {
+                    $data = json_decode( file_get_contents( '/srv/config/tideways.json' ) );
+                    if ( !empty( $data ) && in_array( $_SERVER['HTTP_HOST'], $data ) {
+                        $is_vvv_tideways = true;
+                    }
                 }
             }  
             catch (\JsonException $exception) {  
